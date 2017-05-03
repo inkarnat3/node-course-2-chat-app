47615bb70f0abc849e07784985f77c41a220b9a8
Matt Walling <inkarnat3@gmail.com>
2017-05-03 16:13:13 -0400

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,7 @@
 var socket = io();
 
     socket.on('connect', () => {
-    console.log('Connected to server');
-   
-    socket.emit('createMessage', {
-        from: 'JoeBlow@BlowingStuff.com',
-        text: 'Got anything for me to blow? ;)'
-    });
+    console.log('Connected to server');    
 });
 
     socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -13,15 +13,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User connected');
-
-    socket.emit('newMessage', {
-        from: 'matt@mydomain.com',
-        text: 'What is up chicken butt?',
-        createdAt: 500
-    });
-
     socket.on('createMessage', (message) => {
-        console.log('New Message:', message);
+        //console.log('New Message:', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
